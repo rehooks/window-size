a800f36ae862d36dcd65b8c3da7cce9c81d20f0c
Camron Flanders <camron.flanders@gmail.com>
2018-10-26 14:02:40 -0500

    fix formatting

--- example.js
+++ example.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import useWindowWidth from './'
+import React from 'react';
+import { render } from 'react-dom';
+import useWindowWidth from './';
 
 function App() {
-    let debouncedWindowWidth = useWindowWidth(500)
-    let windowWidth = useWindowWidth()
-    return [
-        <pre>Debounced: {JSON.stringify(debouncedWindowWidth)}</pre>,
-        <pre>Firehose: {JSON.stringify(windowWidth)}</pre>,
-    ]
+	let debouncedWindowWidth = useWindowWidth(500);
+	let windowWidth = useWindowWidth();
+	return [
+		<pre>Debounced: {JSON.stringify(debouncedWindowWidth)}</pre>,
+		<pre>Firehose: {JSON.stringify(windowWidth)}</pre>,
+	];
 }
 
-render(<App />, window.root)
+render(<App />, window.root);
