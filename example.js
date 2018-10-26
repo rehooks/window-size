5ce1a9b9733d7eb0c9bf31e91e3312af02dff37d
Camron Flanders <camron.flanders@gmail.com>
2018-10-26 14:12:45 -0500

    fix formatting

--- example.js
+++ example.js
@@ -3,12 +3,12 @@ import { render } from 'react-dom';
 import useWindowWidth from './';
 
 function App() {
-	let debouncedWindowWidth = useWindowWidth(500);
-	let windowWidth = useWindowWidth();
-	return [
-		<pre>Debounced: {JSON.stringify(debouncedWindowWidth)}</pre>,
-		<pre>Firehose: {JSON.stringify(windowWidth)}</pre>,
-	];
+  let debouncedWindowWidth = useWindowWidth(500);
+  let windowWidth = useWindowWidth();
+  return [
+    <pre>Debounced: {JSON.stringify(debouncedWindowWidth)}</pre>,
+    <pre>Firehose: {JSON.stringify(windowWidth)}</pre>,
+  ];
 }
 
 render(<App />, window.root);
--- index.js
+++ index.js
@@ -2,37 +2,37 @@
 let { useState, useEffect } = require('react');
 
 function getSize() {
-	return {
-		innerHeight: window.innerHeight,
-		innerWidth: window.innerWidth,
-		outerHeight: window.outerHeight,
-		outerWidth: window.outerWidth,
-	};
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth,
+  };
 };
 
 function useWindowSize(debounceMs) {
-	let [windowSize, setWindowSize] = useState(getSize());
+  let [windowSize, setWindowSize] = useState(getSize());
 
-	let timeoutId;
+  let timeoutId;
 
-	function handleResize() {
-		if (timeoutId) {
-			clearTimeout(timeoutId);
-		}
+  function handleResize() {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
 
-		timeoutId = setTimeout(function() {
-			setWindowSize(getSize());
-		}, debounceMs);
-	}
+    timeoutId = setTimeout(function() {
+      setWindowSize(getSize());
+    }, debounceMs);
+  }
 
-	useEffect(() => {
-		window.addEventListener('resize', handleResize);
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
-	}, []);
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
-	return windowSize;
+  return windowSize;
 }
 
 module.exports = useWindowSize;
