2b8f8a2e0e607dc3f8b552426a4c0bd75c174ef7
Sebastian Ekström <seb.ekstrom@gmail.com>
2019-02-19 11:47:57 +0100

    Check if SSR

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 'use strict';
 let { useState, useEffect } = require('react');
 
-function getSize() {
-  return {
-    innerHeight: window.innerHeight,
-    innerWidth: window.innerWidth,
-    outerHeight: window.outerHeight,
-    outerWidth: window.outerWidth,
-  };
-}
-
 function useWindowSize() {
+  let isClient = typeof window === 'object';
   let [windowSize, setWindowSize] = useState(getSize());
+
+  function getSize() {
+    return {
+      innerHeight: isClient ? window.innerHeight : undefined,
+      innerWidth: isClient ? window.innerWidth : undefined,
+      outerHeight: isClient ? window.outerHeight : undefined,
+      outerWidth: isClient ? window.outerWidth : undefined
+    };
+  }
 
   function handleResize() {
     setWindowSize(getSize());
   }
 
   useEffect(() => {
+    if (!isClient) return undefined;
+
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
