64a80a64da183f64bda35224175c8e94a656876e
Federico Zivolo <federico.zivolo@gmail.com>
2019-08-15 12:26:40 +0200

    feat: don't mutate windowSize object if sizes don't change

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 'use strict';
-let { useState, useEffect } = require('react');
+let { useState, useEffect, useMemo } = require('react');
 
-function getSize() {
-  return {
+function useWindowSize() {
+  const getSize = useMemo(() => () => ({
     innerHeight: window.innerHeight,
     innerWidth: window.innerWidth,
     outerHeight: window.outerHeight,
     outerWidth: window.outerWidth,
-  };
-}
-
-function useWindowSize() {
+  }), [
+    window.innerHeight,
+    window.innerWidth,
+    window.outerHeight,
+    window.outerWidth,
+  ]);
+  
   let [windowSize, setWindowSize] = useState(getSize());
 
   function handleResize() {
